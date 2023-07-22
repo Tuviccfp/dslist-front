da8c1484fdf927c98e3eebc1438dd6fefeda26e6
Tuviccfp <victorcordeirofp@gmail.com>
2023-07-22 19:03:12 -0300

    Creation of the game component and connected API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Game, { Games } from './components/Game';
 
 function App() {
+  const [dataGame, setDataGame] = useState<Games[]>([]);
+  
+  async function getGames() {
+    try {
+      const result = await axios.get<Games[]>('http://localhost:8080/games')
+      setDataGame(result.data)
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+    getGames();
+  }, [])
+
+  console.log(dataGame);
+
   return (
     <div className="App">
-      Olá mundo
+      <Game Games={dataGame}/>
     </div>
   );
 }
